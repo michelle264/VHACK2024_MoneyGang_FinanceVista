e766b131d4ea9e8a2447cac60d196b41a2a9a608
Michelle Hu Ynh Tin <116173980+michelle264@users.noreply.github.com>
2024-03-16 23:56:58 +0800

    upload

--- src/components/NavBar/navbarReal.js
+++ src/components/NavBar/navbarReal.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { VscAccount } from "react-icons/vsc";
-import './navbarReal';
+import './navbarReal.css';
 import icon from './icon.png';
-import profileImage from './profile.png';
 
 
 const NavigationBar=() =>{
@@ -20,11 +19,14 @@ const NavigationBar=() =>{
           </div>
 
         </div>
+        <div className='topContainer'>
         <div className="menu">
           <Link to="/about">
             {/* <FaBriefcase className="small-icon"/> */}
             <p className="menu-text">About FinanceVista</p>
           </Link>
+          
+          <div className='line'></div>
           <Link to="/course">
           <div className="small-icon3">
           {/* <img src={profileImage} alt="profile"/>    */}
@@ -32,22 +34,23 @@ const NavigationBar=() =>{
             </div>
           </Link>
 
+          <div className='line'></div>
           <Link to="/market">
           <div className="small-icon4">
           {/* <img src={profileImage} alt="profile"/>    */}
           <p className="market-text">Market Data</p>
             </div>
           </Link>
-
-          <div className="profile">
-          <VscAccount className="profile-icon" style={{ fontSize: '24px', marginTop: '15px', marginRight:'10px' }}/> 
+          </div>
           </div>
 
-          <Link to="/">
+          <Link to="/" className="logout-link">
+          <VscAccount className="profile-icon" style={{ fontSize: '24px', marginTop: '5px', marginRight:'10px' }}/> 
             <i className="small-icon">&#128274;</i>
-            <p className="menu-text">Logout</p>
-          </Link>
-        </div>
+            <p className="menu-text2">Logout</p>
+        </Link>
+
+        
       </nav>
     );
   };
